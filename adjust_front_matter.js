513b72eb389df958885da119c1f1a017f02dac9b
Fabian Wetzel <fabsenet@googlemail.com>
2017-03-02 21:22:18 +0100

    the script should download external images

--- adjust_front_matter.js
+++ adjust_front_matter.js
@@ -1,7 +1,12 @@
 var recursive = require('recursive-readdir');
 var fs = require('fs');
-// var parser = require('parser-front-matter');
+var path = require('path');
+var url = require('url');
+var http = require('http');
+var https = require('https');
 var matter = require('gray-matter');
+
+
 recursive("./source/_posts/", (err, files) => {
     files.forEach(function (file) {
         // console.log(file);
@@ -18,10 +23,38 @@ recursive("./source/_posts/", (err, files) => {
             if (!postObject.data.author) {
                 postObject.data.author = "Fabian Wetzel";
             }
+            delete postObject.data.id;
+            delete postObject.data.comment;
 
             //sync description (feed) to subtitle (index page)
             if (postObject.data.subtitle && !postObject.data.description) {
                 postObject.data.description = postObject.data.subtitle;
+            }
+
+            var externalImageRegex = /[^\)]\]\((http[s]?:\/\/az275061\.vo\.msecnd\.net\/blogmedia[^\)\s]*)/g;
+            var externalImageMatch;
+            while (externalImageMatch = externalImageRegex.exec(postObject.content)) {
+                //this post has images which should be downloaded
+                var externalUrl = externalImageMatch[1];
+                var fileDetails = path.parse(file);
+                var urlDetails = path.parse(externalUrl)
+                var targetFile = path.join(fileDetails.dir, fileDetails.name, urlDetails.base);
+
+                console.log("making sure, target dir exists: " + path.join(fileDetails.dir, fileDetails.name));
+                fs.mkdirSync(path.join(fileDetails.dir, fileDetails.name));
+
+                console.log("found a remote image: " + externalUrl);
+                console.log("going to download it to: " + targetFile);
+
+                var localImageFile = fs.createWriteStream(targetFile);
+                var protocolDownloader = externalUrl.startsWith("https:") ? https : http;
+                var request = protocolDownloader.get(externalUrl, function (response, err) {
+                    if (err) {
+                        console.error(err);
+                        return;
+                    }
+                    response.pipe(localImageFile);
+                });
             }
 
             //write back
