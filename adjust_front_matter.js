6c7191ec21a8a894a6eec1e4a5484351414b52c4
Fabian Wetzel <fabsenet@googlemail.com>
2017-03-17 21:29:58 +0100

    var => let

--- adjust_front_matter.js
+++ adjust_front_matter.js
@@ -1,16 +1,16 @@
-var recursive = require('recursive-readdir');
-var fs = require('fs');
-var path = require('path');
-var url = require('url');
-var http = require('http');
-var https = require('https');
-var matter = require('gray-matter');
-var mkdirp = require('mkdirp');
+let recursive = require('recursive-readdir');
+let fs = require('fs');
+let path = require('path');
+let url = require('url');
+let http = require('http');
+let https = require('https');
+let matter = require('gray-matter');
+let mkdirp = require('mkdirp');
 
 recursive("./source/", (err, files) => {
     files.forEach(function (file) {
 
-        if(!file.endsWith("index.md")) return;
+        //if(!file.endsWith("sensitive.md")) return;
         // console.log(file);
 
         fs.readFile(file, (err, fileContentBuffer) => {
@@ -19,7 +19,7 @@ recursive("./source/", (err, files) => {
                 return;
             }
             console.log("working on " + file);
-            var postObject = matter(fileContentBuffer.toString());
+            let postObject = matter(fileContentBuffer.toString());
 
             //actual modify
             if (!postObject.data.author) {
@@ -33,14 +33,14 @@ recursive("./source/", (err, files) => {
                 postObject.data.description = postObject.data.subtitle;
             }
 
-            var externalImageRegex = /(http[s]?:\/\/az275061\.vo\.msecnd\.net\/blogmedia[^\)\s]*)/gi;
-            var externalImageMatch;
+            let externalImageRegex = /(http[s]?:\/\/az275061\.vo\.msecnd\.net\/blogmedia[^\)\s]*)/gi;
+            let externalImageMatch;
             while (externalImageMatch = externalImageRegex.exec(postObject.content)) {
                 //this post has images which should be downloaded
-                var externalUrl = externalImageMatch[1];
-                var fileDetails = path.parse(file);
-                var urlDetails = path.parse(externalUrl)
-                var targetFile = path.join(fileDetails.dir, fileDetails.name, urlDetails.base);
+                let externalUrl = externalImageMatch[1];
+                let fileDetails = path.parse(file);
+                let urlDetails = path.parse(externalUrl)
+                let targetFile = path.join(fileDetails.dir, fileDetails.name, urlDetails.base);
 
                 mkdirp(path.join(fileDetails.dir, fileDetails.name), function (err) {
 
@@ -48,9 +48,9 @@ recursive("./source/", (err, files) => {
                     console.log("found a remote image: " + externalUrl);
                     console.log("going to download it to: " + targetFile);
 
-                    var localImageFile = fs.createWriteStream(targetFile);
-                    var protocolDownloader = externalUrl.startsWith("https:") ? https : http;
-                    var request = protocolDownloader.get(externalUrl, function (response, err) {
+                    let localImageFile = fs.createWriteStream(targetFile);
+                    let protocolDownloader = externalUrl.startsWith("https:") ? https : http;
+                    let request = protocolDownloader.get(externalUrl, function (response, err) {
                         if (err) {
                             console.error(err);
                             return;
@@ -63,8 +63,8 @@ recursive("./source/", (err, files) => {
             }
 
             //write back
-            var content = postObject.content;
-            var newFileContent = matter.stringify(postObject.content, postObject.data);
+            let content = postObject.content;
+            let newFileContent = matter.stringify(postObject.content, postObject.data);
 
             fs.writeFile(file, newFileContent, err => { if (err) console.error(err) });
         });
